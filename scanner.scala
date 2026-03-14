92f532dab5f0f4e655976cb8c97efb1cb8562817
kwadwoz <zenasboamah18@gmail.com>
2026-03-13 23:51:12 -0400

    Some progress with implementing a scanner

---
+++ scanner.scala
@@ -0,0 +1,25 @@
+
+
+
+enum TokenType:
+  // Single-character tokens
+  case LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR
+  // One or two character tokens
+  case BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL
+  // Literals 
+  case IDENTIFIER, STRING, NUMBER
+  // Keywords 
+  case AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE
+  case EOF
+
+case class Token(
+    tokenType: TokenType,
+    lexeme: String, 
+    literal: Any,
+    line: Int
+)
+
+@main def runScannerTest(): Unit =
+    val myToken = Token(TokenType.PLUS,"+",null,1 )
+
+    println(myToken)
